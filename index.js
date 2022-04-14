93eeede67175f68eabf996c9e55fb6e1720bfe9b
TechnoDotΩ <73961295+TechDudie@users.noreply.github.com>
2022-04-14 18:20:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 console.log('welcome to JavaScript');
 console.log('LOL');
 console.log('Ever wondered why you exist?');
+alert("im sorry that it sucks");
+alert("2BU!");
+alert("To all the coding noobs out there");
+alert("The quick brown fox jumped over the lazy dog. Hello World!");
