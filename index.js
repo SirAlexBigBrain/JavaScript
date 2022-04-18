cacd31db08d2c0b8fb0330a96d8478172c0af5dd
Α Αλέξανδρος Ω <98426972+SirAlexBigBrain@users.noreply.github.com>
2022-04-18 08:07:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ alert("im sorry that it sucks");
 alert("2BU!");
 alert("To all the coding noobs out there");
 alert("The quick brown fox jumped over the lazy dog. Hello World!");
+alert("Hardwaregore, why you log on to my account and edit? ****!");
