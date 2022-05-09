ee60da940ed5f1f5f4f9914c34d32e3b0e0d0527
Α Αλέξανδρος Ω <98426972+SirAlexBigBrain@users.noreply.github.com>
2022-05-09 11:49:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ console.log('welcome to JavaScript');
 console.log('LOL');
 console.log('Ever wondered why you exist?');
 alert("im sorry that it sucks");
+function myFunction(p1, p2) {
+  return p1 * p2;   // The function returns the product of p1 and p2
+}
 alert("2BU!");
 alert("To all the coding noobs out there");
 alert("The quick brown fox jumped over the lazy dog. Hello World!");
